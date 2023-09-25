80d522da6ff979866f878072a64915a16a5c9387
Fatin110 <ishmam110@gmail.com>
2023-09-26 00:55:47 +0600

    question added in comment

--- task_2.php
+++ task_2.php
@@ -7,3 +7,10 @@ function filterOutEven ($num){
 
 $evenFilteredOut = array_filter ($numbers, 'filterOutEven');
 print_r ($evenFilteredOut);
+
+/* 
+Task 2: Array Manipulation
+
+
+Create an array called $numbers containing the numbers 1 to 10. Write a PHP function which takes the "$numbers" array as an argument to remove the even numbers from the array and print the resulting array.
+*/
